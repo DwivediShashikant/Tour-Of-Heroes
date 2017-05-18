261e7762d33f4ee3beba19b066de6811cfd3d19f
DwivediShashikant <dwivedi.sk@outlook.com>
2017-05-18 12:28:27 +0530

    restored project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+ export const HEROES : Hero[] = [
+  {id:11, name: "Batman" },
+  {id:12, name: "Superman" },
+  {id:13, name: "Sherlock" },
+  {id:14, name: "Spiderman" },
+  {id:15, name: "Flash" },
+  {id:16, name: "Deadpool" },
+  {id:17, name: "Hulk" },
+  {id:18, name: "Captian America" },
+  {id:19, name: "Iron Man"},
+  {id:20, name: "Thor" }
+]
